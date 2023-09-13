35436e4e590d4c2ee331c156701a9fc4733b36f2
Naleesh Jeffrey <naleeshp@treinetic.com>
2023-09-13 15:14:49 +0530

    Initial Commit

---
+++ Planet/PlanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetApp.swift
+//  Planet
+//
+//  Created by Naleesh Jeffrey on 2023-09-13.
+//
+
+import SwiftUI
+
+@main
+struct PlanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
